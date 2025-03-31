ec27c53d1b690fb80118f799e0b622a09db8072e
sungHyeon <qortjdgus8999@gmail.com>
2025-03-31 17:55:01 +0900

    test_commit

---
+++ src/main/java/com/example/demo/test/controller/BoardController.java
@@ -0,0 +1,28 @@
+package com.example.demo.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    @GetMapping("/board")
+    public String board(Model model) {
+    	model.addAttribute("showHeader", false);
+    	model.addAttribute("currentPage", "community");
+        return "views/board/boardmain";
+    }
+    
+    @GetMapping("/boardfollowing")
+    public String boardfollowing(Model model) {
+    	model.addAttribute("currentPage","community");
+        return "views/board/boardfollowing";
+    }
+    
+    @GetMapping("/boardmost")
+    public String boardmost(Model model) {
+    	model.addAttribute("currentPage","community");
+        return "views/board/boardmost";
+    }
+    
+}
