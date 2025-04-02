22032e43427056e2c361cfdcade395e3d2380d14
sungHyeon <qortjdgus8999@gmail.com>
2025-04-02 18:05:49 +0900

    second_commit

--- src/main/java/com/example/demo/board/controller/BoardController.java
+++ src/main/java/com/example/demo/board/controller/BoardController.java
@@ -1,4 +1,4 @@
-package com.example.demo.test.controller;
+package com.example.demo.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,20 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
-    @GetMapping("/board")
+    @GetMapping("/feed/all")
     public String board(Model model) {
     	model.addAttribute("showHeader", false);
     	model.addAttribute("currentPage", "community");
         return "views/board/boardmain";
     }
     
-    @GetMapping("/boardfollowing")
+    @GetMapping("/feed/following")
     public String boardfollowing(Model model) {
+    	model.addAttribute("showHeader", false);
     	model.addAttribute("currentPage","community");
         return "views/board/boardfollowing";
     }
     
-    @GetMapping("/boardmost")
+    @GetMapping("/feed/popular")
     public String boardmost(Model model) {
     	model.addAttribute("currentPage","community");
         return "views/board/boardmost";
