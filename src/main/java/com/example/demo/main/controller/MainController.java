682af5ced84f00f38d058866563c78b8e46473d4
groom2hub <groomwnsgur35@gmail.com>
2025-04-15 13:58:12 +0900

    remove mainpage chart

--- src/main/java/com/example/demo/main/controller/MainController.java
+++ src/main/java/com/example/demo/main/controller/MainController.java
@@ -4,32 +4,32 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 import java.sql.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.board.service.BoardImageService;
 import com.example.demo.board.service.BoardService;
 import com.example.demo.dto.board.BoardDto;
+import com.example.demo.dto.board.BoardImageDto;
 import com.example.demo.dto.user.UserDto;
 import com.example.demo.record.service.ExerciseDetailService;
 import com.example.demo.record.service.RecordService;
 import com.example.demo.user.service.UserService;
 import com.example.demo.util.CalorieCalculator;
-import com.example.demo.util.CalorieGraphGenerator;  // CalorieGraphGenerator 추가
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
     private final UserService userService;
     private final BoardService boardService;
+    private final BoardImageService boardImageService;
     private final RecordService recordService;
     private final ExerciseDetailService exerciseDetailService;
-    @Autowired
-    private CalorieGraphGenerator calorieGraphGenerator;
     
     @GetMapping("/mainpage")
     public String searchFollower(HttpSession session, Model model) {
@@ -41,14 +41,8 @@ public class MainController {
         }
         UserDto user = userService.getUserById(userId);
         
-        List<BoardDto> boardList = boardService.getBoardPreview();
-        model.addAttribute("boardList", boardList);
-        
         // targetCalories를 model에 추가
         int targetCalories = CalorieCalculator.calculateTargetCalories(user);
-
-        // targetCalories를 CalorieGraphGenerator에 전달
-        calorieGraphGenerator.generateGraph(userId, targetCalories); // 사용자 ID와 targetCalories 전달
 
         model.addAttribute("targetCalories", targetCalories);
         model.addAttribute("AMR", CalorieCalculator.calculateAMR(user));
@@ -63,7 +57,31 @@ public class MainController {
         int burnedKcalByExercise = exerciseDetailService.getTotalBurned(userId, date);
         model.addAttribute("burnedKcalByExercise", burnedKcalByExercise);
         model.addAttribute("leftKcal", targetCalories - Integer.parseInt(todayFoodRecord.get("total").replace("Kcal", "")) + burnedKcalByExercise);
+        
+        List<BoardDto> posts = boardService.getBoardPreview();
+        Map<BoardDto, String> postsMap = new LinkedHashMap<>();
 
+        for (BoardDto post : posts) {
+        	String imageUrl;
+        	System.out.println(post.getId());
+        	try {
+        		List<BoardImageDto> boardImage = boardImageService.getImagesByBoardId(11);
+        		if (boardImage != null && !boardImage.isEmpty()) {
+                    imageUrl = boardImage.get(0).getFileName();
+                } else {
+                    imageUrl = null;
+                }
+        	} catch (Exception e) {
+        		imageUrl = null;
+			}
+        	System.out.println(imageUrl);
+
+            postsMap.put(post, imageUrl);
+        }
+
+        model.addAttribute("postsMap", postsMap);
+
+        
         return "views/mainpage/mainpage";
     }
 }
