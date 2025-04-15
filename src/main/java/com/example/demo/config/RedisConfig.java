414f82d01295255012bb2c0d495fbe35ef2d01a6
ChoiWonOong <dnd5301@gmail.com>
2025-04-15 17:07:19 +0900

    0415 1700

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,10 +12,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
+    @Value("${spring.redis.host}")
+    private String REDIS_HOST;
+    @Value("${spring.redis.port}")
+    private int REDIS_PORT;
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+        return new LettuceConnectionFactory(REDIS_HOST, REDIS_PORT);
     }
 
     @Bean
